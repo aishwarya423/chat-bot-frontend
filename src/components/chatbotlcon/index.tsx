e9e94df992a63215b6eba6dc50b73b1ae41a629b
Aishwarya <76626494+aishwarya423@users.noreply.github.com>
2025-07-22 19:50:19 +0530

    Update index.tsx

---
+++ src/components/chatbotlcon/index.tsx
@@ -0,0 +1,13 @@
+import chatIcon from "../../assets/chatbot.svg"
+
+const ChatbotIcon:React.FC=()=>{
+    return(
+        <a href="https://react.dev" target="_blank">
+          <img src={chatIcon} className="logo react" alt="React logo" />
+        </a> 
+    )
+}
+
+
+export default ChatbotIcon;
+ult Chatbot;
