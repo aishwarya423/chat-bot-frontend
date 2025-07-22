bc4157f0c85f264a937fff023d6fcb6ccc436f4f
Aishwarya <76626494+aishwarya423@users.noreply.github.com>
2025-07-22 20:04:48 +0530

    Create mock/Server.ts

---
+++ src/mock/Server.ts
@@ -0,0 +1,19 @@
+// import axios from 'axios';
+
+// axios.interceptors.request.use((config) => {
+//   if (config.url === 'https://your-backend-api.com/endpoint' && config.method === 'post') {
+//     return Promise.resolve({
+//       data: {
+//         success: true,
+//         message: 'Intercepted mock response',
+//         data: { pensionAmount: 1000, currency: 'GBP' },
+//       },
+//       status: 200,
+//       statusText: 'OK',
+//       headers: {},
+//       config,
+//     });
+//   }
+//   return config;
+// });
+//mock//server.ts
